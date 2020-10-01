ba3b34f03903b138e2aa5d0ec66335f3ef4e0049
sergei.sekatski <sergei.sekatski@sap.com>
2020-10-01 11:45:39 +0300

    add solution for 03-streams 02-line-split-stream

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -1,34 +1,28 @@
 const stream = require('stream');
 const os = require('os');
 
-let _currentString;
-
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
-    _currentString = '';
+    this._currentString = '';
   }
 
   _transform(chunk, encoding, callback) {
-    try {
-      const chunks = chunk.toString().split(os.EOL);
-      if (chunks.length > 1) {
-        _currentString += chunks[0];
-        callback(null, _currentString);
-        chunks.shift();
-        _currentString = chunks.join();
-      } else {
-        _currentString += chunks[0];
-        callback();
-      }
-    } catch (e) {
-      // skip
+    const chunks = chunk.toString().split(os.EOL);
+    if (chunks.length > 1) {
+      this._currentString += chunks[0];
+      callback(null, this._currentString);
+      chunks.shift();
+      this._currentString = chunks.join();
+    } else {
+      this._currentString += chunks[0];
+      callback();
     }
   }
 
   _flush(callback) {
-    if (_currentString) {
-      callback(null, _currentString);
+    if (this._currentString) {
+      callback(null, this._currentString);
     }
   }
 }
