cad63f223990ec88765ac4ca0d700d594b8eb0a2
sergei.sekatski <sergei.sekatski@sap.com>
2020-10-05 12:10:24 +0300

    add solution for 06-koajs 01-chat-app

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -8,10 +8,25 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+let _subscriber = 1;
+
 router.get('/subscribe', async (ctx, next) => {
+  const _lastMessage = new Promise((resolve) => {
+    this['_resolveSubscriber' + _subscriber] = resolve;
+  });
+  _subscriber++;
+  ctx.body = await _lastMessage;
 });
 
 router.post('/publish', async (ctx, next) => {
+  const message = ctx.request.body.message;
+  if (message) {
+    for (let i = 1; i < _subscriber; i++) {
+      this['_resolveSubscriber' + i](message);
+    }
+    _subscriber = 1;
+    ctx.status = 201;
+  }
 });
 
 app.use(router.routes());
